bf1446b41ab95f58f773ab75d0956105fc8c2553
Shagun Khemka <shagun.khemka60@gmail.com>
2019-02-13 03:12:01 +0530

    Filter for actors enabled

--- jumla/charchitra/views.py
+++ jumla/charchitra/views.py
@@ -7,11 +7,12 @@ from django.views import generic
 from django.http import HttpResponse
 from .models import *
 
+
 # Create your views here.
 def dashboard(request):
-	return render(request, 'charchitra/dashboard.html')
+    return render(request, 'charchitra/dashboard.html')
 
-	
+
 # class ListVideoView(View):
 #     def get(self,request):
 #         # l=[{"title":"https://images-na.ssl-images-amazon.com/images/I/41kYaOc988L._SL500_AC_SS350_.jpg"},
@@ -27,40 +28,43 @@ def dashboard(request):
 #         l = [ { "title" : "https://images-na.ssl-images-amazon.com/images/I/41kYaOc988L._SL500_AC_SS350_.jpg" }]
 #         return render(request,'charchitra/video_list.html',{'video_list':l})
 
-      
 
 # class VideoListView(generic.ListView):
 # 	model = Video
 # 	template_name = 'charchitra/video_list.html'
-
-def VideoListView(request):
-	if request.method=="GET" and 'search_data' in request.GET:
-		video_list = Video.objects.filter(v_name__contains=request.GET['search_data'])
-	else:
-		video_list = Video.objects.all()
-	print(video_list)
-	video_price_list = VideoPrice.objects.all()
-	print(video_price_list)
-	template_name = 'charchitra/video_list.html'
-	context = {
-	'video_list' : video_list,
-	'video_price_list' : video_price_list,
-	}
-	return render(request, template_name, context)
+def VideoListView(request, video_list=[]):
+    if request.method == "GET":
+        if 'filter_actor' in request.GET:
+            video_list = Video.objects.filter(a_id__a_name=request.GET['filter_actor'])
+        elif 'filter_genre' in request.GET: # WIP
+            video_list = Video.objects.filter(g_id__g_name=request.GET['filter_genre'])
+        elif 'search_data' in request.GET:
+            video_list = Video.objects.filter(v_name__contains=request.GET['search_data'])
+        else:
+            video_list = Video.objects.all()
+    print(video_list)
+    video_price_list = VideoPrice.objects.all()
+    print(video_price_list)
+    template_name = 'charchitra/video_list.html'
+    context = {
+        'video_list': video_list,
+        'video_price_list': video_price_list,
+    }
+    return render(request, template_name, context)
 
 
 def VideoDetailView(request, video_id):
-	video_detail = get_object_or_404(Video, pk=video_id)
-	print(video_detail)
-	video_price_detail = VideoPrice.objects.filter(v_id=video_id)
-	print(video_price_detail)
-	template_name = 'charchitra/video_detail.html'
-	context = {
-		'video_detail' : video_detail,
-		'video_price_detail' : video_price_detail,
-	}
-	return render(request, template_name, context)
+    video_detail = get_object_or_404(Video, pk=video_id)
+    print(video_detail)
+    video_price_detail = VideoPrice.objects.filter(v_id=video_id)
+    print(video_price_detail)
+    template_name = 'charchitra/video_detail.html'
+    context = {
+        'video_detail': video_detail,
+        'video_price_detail': video_price_detail,
+    }
+    return render(request, template_name, context)
 
 
 def VideoPackListView(request):
-	HttpResponse("Welcome to Video Packs.")
+    HttpResponse("Welcome to Video Packs.")
