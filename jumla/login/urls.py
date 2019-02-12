1259e3a178d65e23e7a588b2d8ee9f7a135491b3
MMuthukrishnan <muthukrishna.m@instamojo.com>
2019-02-12 12:59:52 +0530

    Login frontend

--- jumla/login/urls.py
+++ jumla/login/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+
 app_name = 'login'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+    url(r'$',views.LoginClass.as_view(),name='LoginClass')
 ]
