0a521295c578424ff4e06675d5ab8ed65fc53eea
Shagun Khemka <shagun.khemka60@gmail.com>
2019-02-12 16:23:16 +0530

    Change db to postgre

--- jumla/jumla/settings.py
+++ jumla/jumla/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -76,10 +78,8 @@ WSGI_APPLICATION = 'jumla.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(env='DATABASE_URL', conn_max_age=120,
+                                      default='postgres://imtestuser:imtestuser@127.0.0.1/imtestweb'),
 }
 
 
