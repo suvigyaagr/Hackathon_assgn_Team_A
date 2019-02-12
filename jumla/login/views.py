2b378305586fb3a5f3a831ec42876abab790f631
M Muthukrishna <47133164+MMuthukrishna@users.noreply.github.com>
2019-02-12 13:03:02 +0530

    Login frontend (#3)

--- jumla/login/views.py
+++ jumla/login/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from django.views import View
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the login page.")
+
+class LoginClass(View):
+    def get(self,request):
+        template = loader.get_template('login/index.html')
+        return HttpResponse(template.render(request))
