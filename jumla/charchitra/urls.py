bbfad2dc356ad0d5c59019875c4a5fa3ea0677b8
Suvigya Agrawal <suvigyaagrawal@Suvigyas-MacBook-Pro.local>
2019-02-12 16:35:43 +0530

    Video initial view

--- jumla/charchitra/urls.py
+++ jumla/charchitra/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = 'charchitra'
 urlpatterns = [
-    url(r'^$',views.dashboard, name='dashboard')
+    url(r'^$',views.dashboard, name='dashboard'),
+    url(r'^videos/$', views.VideoListView.as_view(), name='video-list'),
+    url(r'^videos/(?P<pk>\d+)/$', views.VideoDetailView.as_view(), name = 'video-detail'),
+
 ]
--- jumla/charchitra/views.py
+++ jumla/charchitra/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 # from django.views import generics
 from django.http import HttpResponse
+from .models import *
+
 
 # Create your views here.
 def dashboard(request):
 	return HttpResponse("Welcome")
+
+
+class VideoView(generic.ListView):
+	model = Video
+	template_name = 'charchitra/video_list.html'
+
+	def get_queryset(self):
+		return Video.objects.all()
+
+
+class
