bc1d1d66236235def20ae9202a4a3c25a9f12a61
Suvigya Agrawal <suvigyaagrawal@Suvigyas-MBP.Dlink>
2019-02-13 12:20:51 +0530

    Making minor changes and solved user_id problem and dumpdata

--- jumla/charchitra/urls.py
+++ jumla/charchitra/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'charchitra'
 urlpatterns = [    
     # url(r'^abc$',views.ListVideoView.as_view(), name='list_video'),
-    url(r'^dashboard/$',views.dashboard, name='dashboard'),
-    url(r'^video/(?P<id>\w+)/$', views.VideoListView, name='video_list'),
-    url(r'^video/(?P<id>\w+)/(?P<video_id>\d+)/', views.VideoDetailView, name='video_detail'),
-    url(r'^videopack/$', views.VideoPackListView, name='video_pack_list'),
+    url(r'^dashboard/(?P<user_id>\w+)/$',views.dashboard, name='dashboard'),
+    url(r'^video/(?P<user_id>\w+)/$', views.VideoListView, name='video_list'),
+    url(r'^video/(?P<user_id>\w+)/(?P<video_id>\d+)/', views.VideoDetailView, name='video_detail'),
+    url(r'^videopack/(?P<user_id>\w+)/$', views.VideoPackListView, name='video_pack_list'),
 ]
